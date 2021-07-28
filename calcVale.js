98b2dce8d0c69c6d4de1c77ee4080d3e7f4623e4
Lucas Carneiro <carneirolucas11@gmail.com>
2021-07-27 21:34:01 -0300

    Add Vale
    Adicionado todos os pieres da Vale

--- calcVale.js
+++ calcVale.js
@@ -13,7 +13,7 @@ function calcVale(ciclo, hora, dwt, calado, tipo, pier, bordo, amp, hb) {
         
         */
 
-  if (pier == p1) {
+  if (pier == P1) {
     if (tipo == EA) {
       if (bordo == BE) {
         //Regra 1
@@ -87,4 +87,178 @@ function calcVale(ciclo, hora, dwt, calado, tipo, pier, bordo, amp, hb) {
       }
     }
   }
+
+  if (pier == P3N) {
+    if (tipo == EA) {
+      if (bordo == BE) {
+        //Regra 1
+        if (ciclo == BM) {
+          //hora = hora
+        }
+        if (ciclo == PM) {
+          //hora = hora - 2horas
+        }
+
+        //Regra 2
+        if (dwt > 210000 && ciclo == BM) {
+          //hora = hora
+        }
+        if (dwt > 210000 && ciclo == PM) {
+          //hora = hora - 2 horas
+        }
+      }
+    }
+    if (tipo == DS) {
+      if (bordo == BE) {
+        //Regra 1
+        if (ciclo == BM) {
+          //hora = hora
+          //alert BM até 1h antes da PM (janela)
+        }
+        if (ciclo == PM) {
+          //hora = hora - 2 horas
+        }
+      }
+    }
+  }
+
+  if (pier == P3S) {
+    if (tipo == EA) {
+      if (bordo == BE) {
+        //Regra 1
+        if (dwt <= 210000 && ciclo == BM) {
+          //hora = hora
+        }
+        if (dwt <= 210000 && ciclo == PM && amp <= 4, 5) {
+          //hora = hora - 1h e 30
+        }
+
+        //Regra 2
+        if (dwt > 210000 && ciclo == BM) {
+          //hora = hora
+        }
+        if (dwt > 210000 && ciclo == PM) {
+          //hora = hora - 2 horas
+        }
+      }
+
+      if (bordo == BB) {
+        //Regra 3
+        if (dwt <= 210000 && ciclo == BM) {
+          //hora = hora - 1h e 30
+          //alert 1 hora até 30mins antes da BM
+        }
+        if ((dwt <= 210000 && ciclo == PM && amp <= 4, 5)) {
+          //hora = hora - 1h e 30
+          //alert 1 hora e 30mins antes da PM até a PM
+        }
+      }
+    }
+    if (tipo == DS) {
+      if (bordo == BE) {
+        //Regra 1
+        if ((dwt <= 210000 && ciclo == BM && amp <= 4, 5)) {
+          //hora = hora
+          //alert BM até 1h antes da PM (janela)
+        }
+        if ((dwt <= 210000 && ciclo == PM && amp <= 4, 5)) {
+          //hora = hora - 1hora e 30mins 
+          //alert BM até 1h antes da PM (janela)
+        }
+
+        //Regra 1 // Segunda AMP
+        if ((dwt <= 210000 && ciclo == BM && amp > 4, 5)) {
+          //hora = hora
+          //alert BM até 1h antes da PM (janela)
+        }
+        if ((dwt <= 210000 && ciclo == PM && amp > 4, 5)) {
+          //hora = hora - 2 hora
+          //alert BM até 1h antes da PM (janela)
+        }
+
+        //Regra 2
+        if ((dwt > 210000 && ciclo == BM && amp > 4, 5)) {
+          //hora = hora
+        }
+        if ((dwt > 210000 && ciclo == PM && amp > 4, 5)) {
+          //hora = hora - 2 horas
+        }
+      }
+
+      if (bordo == BB) {
+        //Regra 4
+        if (dwt < 210000 && ciclo == BM) {
+          //hora = hora - 1 horas
+          //alert 1h antes da BM até 30mins antes a BM(janela)
+        }
+        if ((dwt < 210000 && ciclo == BM && amp <= 4, 5)) {
+          //hora = hora - 1h e 30mins
+          //alert 1h e 30mins antes da PM até a PM
+        }
+      }
+    }
+  }
+
+  if (pier == P4S) {
+    if (tipo == EA) {
+      if (bordo == BE) {
+        //Regra 1
+        if (ciclo == BM) {
+          //hora = hora
+        }
+        if (ciclo == PM) {
+          //hora = hora - 2horas
+        }
+      }
+    }
+    if (tipo == DS) {
+      if (bordo == BE) {
+        //Regra 1
+        if (ciclo == BM) {
+          //hora = hora
+        }
+        if (ciclo == PM) {
+          //hora = hora - 2 horas
+        }
+      }
+    }
+  }
+
+  if (pier == P4N) {
+    if (tipo == EA) {
+      if (bordo == BE) {
+        //Regra 1
+        if (ciclo == BM) {
+          //hora = hora
+        }
+        if (ciclo == PM) {
+          //hora = hora - 2horas
+        }
+      }
+    }
+    if (tipo == DS) {
+      if (bordo == BE) {
+        //Regra 1
+        if (dwt <= 330000 && ciclo == BM) {
+          //hora = hora
+          //alert ( Com simultanea 30 mins antes da BM)
+        }
+        if (dwt <= 330000 && ciclo == PM) {
+          //hora = hora - 2 horas
+        }
+
+        //Regra 2
+        if (dwt > 330000 && ciclo == BM) {
+          //hora = hora
+          //alert ( Com simultanea 30 mins antes da BM)
+        }
+        if (dwt > 330000 && ciclo == PM) {
+          //hora = hora - 1hora e 30mins
+        }
+      }
+    }
+  }
+
+
+
 }
